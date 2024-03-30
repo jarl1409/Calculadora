6fcf299062b72ad27496f1b6dba190ec96fcbe84
jarl1409 <jhosepandrey203@gmail.com>
2024-03-29 21:14:31 -0500

    empezando de nuevo el JS

--- js/operadores.js
+++ js/operadores.js
@@ -1,6 +1,26 @@
-document.getElementById("suma").addEventListener('click', () => setOperador('+'));
-document.getElementById("resta").addEventListener('click', () => setOperador('-'));
-document.getElementById("multiplicacion").addEventListener('click', () => setOperador('x'));
-document.getElementById("division").addEventListener('click', () => setOperador('÷'));
-document.getElementById("porcentaje").addEventListener('click', () => setOperador('%'));
-document.getElementById("resultado").addEventListener('click', mostrarResultado);
+var operandoA;
+var operandoB;
+var operacion;
+
+var resultado;
+
+function init() {
+  var uno = document.getElementById("1");
+  var dos = document.getElementById("2");
+  var tres = document.getElementById("3");
+  var cuatro = document.getElementById("4");
+  var cinco = document.getElementById("5");
+  var seis = document.getElementById("6");
+  var siete = document.getElementById("7");
+  var ocho = document.getElementById("8");
+  var nueve = document.getElementById("9");
+  var cero = document.getElementById("0");
+  var suma = document.getElementById("suma");
+  var resta = document.getElementById("resta");
+  var multiplicacion = document.getElementById("multiplicacion");
+  var division = document.getElementById("division");
+  var porcentaje = document.getElementById("porcentaje");
+  resultado = document.getElementById("resultado");
+  var reset = document.getElementById("reset");
+  var eliminar = document.getElementById("eliminar");
+}
