22577fcb6409efbeb0ea84486e100feb439d1808
jarl1409 <jhosepandrey203@gmail.com>
2024-04-02 14:45:13 -0500

    Cambie el script, pero aun me sale error, estoy buscando como arreglarlo

--- js/operadores.js
+++ js/operadores.js
@@ -21,62 +21,101 @@ function init() {
   var igual = document.getElementById("igual");
   var reset = document.getElementById("reset");
   var eliminar = document.getElementById("eliminar");
-
-  uno.onclick = function () {
-    resultado.textContent += "1";
-  };
-  dos.onclick = function () {
-    resultado.textContent += "2";
-  };
-  tres.onclick = function () {
-    resultado.textContent += "3";
-  };
-  cuatro.onclick = function () {
-    resultado.textContent += "4";
-  };
-  cinco.onclick = function () {
-    resultado.textContent += "5";
-  };
-  seis.onclick = function () {
-    resultado.textContent += "6";
-  };
-  siete.onclick = function () {
-    resultado.textContent += "7";
-  };
-  ocho.onclick = function () {
-    resultado.textContent += +"8";
-  };
-  nueve.onclick = function () {
-    resultado.textContent += "9";
-  };
-  cero.onclick = function () {
-    resultado.textContent += "0";
-  };
-
-  reset.onclick = function () {
-    resetear();
-  };
-  suma.onclick = function () {
-    operandoA = resultado.textContent;
-    operacion = "+";
-    limpiar();
-  };
-  resta.onclick = function () {
-    operandoA = resultado.textContent;
-    operacion = "-";
-    limpiar();
-  };
-  division.onclick = function () {
-    operandoA = resultado.textContent;
-    operacion = "/";
-    limpiar();
-  };
-  multiplicacion.onclick = function () {
-    operandoA = resultado.textContent;
-    operacion = "*";
-    limpiar();
-  };
-  porcentaje.onclick = function () {
-    operacion = "";
-  };
 }
+uno.onclick = function (e) {
+  resultado.textContent += "1";
+};
+dos.onclick = function (e) {
+  resultado.textContent += "2";
+};
+tres.onclick = function (e) {
+  resultado.textContent += "3";
+};
+cuatro.onclick = function (e) {
+  resultado.textContent += "4";
+};
+cinco.onclick = function (e) {
+  resultado.textContent += "5";
+};
+seis.onclick = function (e) {
+  resultado.textContent += "6";
+};
+siete.onclick = function (e) {
+  resultado.textContent += "7";
+};
+ocho.onclick = function (e) {
+  resultado.textContent += +"8";
+};
+nueve.onclick = function (e) {
+  resultado.textContent += "9";
+};
+cero.onclick = function (e) {
+  resultado.textContent += "0";
+};
+
+reset.onclick = function (e) {
+  resetear();
+};
+suma.onclick = function (e) {
+  operandoA = resultado.textContent;
+  operacion = "+";
+  limpiar();
+};
+resta.onclick = function (e) {
+  operandoA = resultado.textContent;
+  operacion = "-";
+  limpiar();
+};
+division.onclick = function (e) {
+  operandoA = resultado.textContent;
+  operacion = "/";
+  limpiar();
+};
+multiplicacion.onclick = function (e) {
+  operandoA = resultado.textContent;
+  operacion = "*";
+  limpiar();
+};
+porcentaje.onclick = function (e) {
+  operandoA = resultado.textContent;
+  operacion = "%";
+};
+igual.onclick = function (e) {
+  operandoB = resultado.textContent;
+  resolver();
+};
+function limpiar() {
+  resultado.textContent = "";
+}
+function resetear() {
+  resultado.textContent = "";
+  operandoA = 0;
+  operandoB = 0;
+  operacion = "";
+}
+
+function resolver() {
+  var res = 0;
+  switch (operacion) {
+    case "+":
+      res = parseFloat(operandoA) + parseFloat(operandoB);
+      break;
+    case "-":
+      res = parseFloat(operandoA) - parseFloat(operandoB);
+      break;
+    case "/":
+      res = parseFloat(operandoA) / parseFloat(operandoB);
+      break;
+    case "*":
+      res = parseFloat(operandoA) * parseFloat(operandoB);
+      break;
+    case "%":
+      res = (parseFloat(operandoA) * parseFloat(operandoB)) / 100;
+  }
+  resetear();
+  resultado.textContent = res;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  init();
+});
