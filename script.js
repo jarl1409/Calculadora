52e48f70718bdc4e60c59522b044cd39f45d48b4
jarl1409 <jhosepandrey203@gmail.com>
2024-04-02 22:05:43 -0500

    Creando nueva calculadora, la otra no funciono

---
+++ script.js
@@ -0,0 +1,5 @@
+const displayValorAnterior = document.getElementById("valor-Anterior");
+const displayValorActual = document.getElementById("valor-Actual");
+const botonesNumeros = document.querySelectorAll("numero");
+const botonesOperadores = document.querySelectorAll("operador");
+
